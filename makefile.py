3f9371861d0f9a4cf1a187576da0296d46c5efee
mskoenz <mskoenz@gmx.net>
2017-04-12 07:54:30 +0200

    added tars
    hash: different people

--- makefile.py
+++ makefile.py
@@ -5,6 +5,9 @@
 # Date:    07.12.2016 14:03:38 CET
 # File:    make.py
 
+from addon import core
+from addon.util import make
+
 
 def job(param, db):
     js = core.namespace()
@@ -28,3 +31,42 @@ def job(param, db):
     js.cmd_param = param
 
     return js
+
+
+@make.out("{build}/data/{opt_path}/maintain.m5c")
+@make.asz("zurkon.launch")
+def launch():
+    make.mkdir("{build}/data/{opt_path}")
+    db = make.parse("{build}/data.s3db")
+
+    param, job = make.assoz_param.output_all
+
+    todo = []
+
+    js = job(dict(input=1), db)
+    todo.append(zurkon.submit(js))
+
+    zurkon.launch_jobs(
+        make.zurkon.output,
+        todo,
+        make.output)
+
+
+@make.out("{build}/data/{opt_path}/maintain.s3db")
+@make.dep("launch")
+@make.asz("zurkon.launch")
+@make.lbl("collect")
+def collect():
+    make.mkdir("{build}/data/{opt_path}")
+    m5c_file = make.depend.output
+    if zurkon.open_jobs(m5c_file) > 0:
+        if not zurkon.collect_jobs(make.assoz.output):  # cancel
+            return make.stop
+
+    util.transfer_table(make.parse("{build}/data.s3db"),
+                        make.output,
+                        m5c_file,
+                        make.opt.bind)
+
+if __name__ == "__main__":
+    make.main()
